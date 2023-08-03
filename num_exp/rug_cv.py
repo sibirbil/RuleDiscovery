1acb581ef35a3611f984e20bc1f607e72667c58a
tabearoeber <t.e.rober@uva.nl>
2023-08-03 20:46:49 +0200

    Update rug_cv.py

--- num_exp/rug_cv.py
+++ num_exp/rug_cv.py
@@ -7,6 +7,12 @@ numCV: int = 5
 testSize = 0.2
 randomState = 21
 RUG_rule_length_cost = True
+RUG_threshold = 0.05
+
+# for fairness results
+# RUG_rule_length_cost = False
+# RUG_threshold = None
+# RUG_record_fairness = True
 
 # CLASSIFICATION
 # binary
@@ -18,6 +24,8 @@ problems = [DS.banknote, DS.hearts, DS.ILPD, DS.ionosphere,
 # multiclass
 # problems = [DS.wine, DS.glass, DS.ecoli, DS.sensorless, DS.seeds]
 
+# fairness results
+# problems = [DS.adult, DS.compas, DS.default, DS.attrition, DS.nursery, DS.student, DS.law]
 
 RUG_pgrid = {'pen_par': [0.1, 1.0, 10.0],
              'max_depth': [3, 5],
@@ -29,4 +37,5 @@ RUG_pgrid = {'pen_par': [0.1, 1.0, 10.0],
 for problem in problems:
     gs_helpers.run(problem, RUG_pgrid, model = 'RUG',
                    randomState = randomState, testSize=testSize, numSplits=numCV,
-                   binary = binary, write=write, RUG_rule_length_cost=RUG_rule_length_cost)
+                   binary = binary, write=write, RUG_rule_length_cost=RUG_rule_length_cost,
+                   RUG_threshold=RUG_threshold, datasets_path='./datasets/datasets/', save_path='./results_w_RUG/')
