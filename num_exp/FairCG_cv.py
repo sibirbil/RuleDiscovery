1c7ccbc163389c846093f5f1781a5599ed49071c
tabearoeber <t.e.rober@uva.nl>
2023-08-03 20:50:37 +0200

    Update FairCG_cv.py

--- num_exp/FairCG_cv.py
+++ num_exp/FairCG_cv.py
@@ -1,15 +1,14 @@
 import grid_search_helpers as gs_helpers
 import Datasets_binary as DS
 
-# binary = True
 binary = False
 write = True
 numCV: int = 5
 testSize = 0.2
 randomState = 21
 fairness_metric = 'EqOfOp' # or 'HammingEqOdd'
+# fairness_metric = 'HammingEqOdd'
 
-# problems = [DS.compas, DS.default, DS.law, DS.attrition, DS.recruitment, DS.student, DS.nursery]
 problems = [DS.compas, DS.adult, DS.default]
 
 CG_pgrid = {'epsilon':[0.025]}
@@ -28,6 +27,6 @@ for problem in problems:
     print(CG_pgrid)
 
     gs_helpers.run(problem, CG_pgrid, model = 'FairCG',
-                randomState = randomState, testSize=testSize, numSplits=numCV, binary = binary, write=write,
+                randomState = randomState, testSize=testSize, numSplits=numCV, binary = False, write=write,
                    data_path='./prepped_data_CG/', fairness_metric=fairness_metric,
-                   datasets_path = './datasets/FairCG_binarized/', save_path = './results_w_FairCG_binary/')
+                   datasets_path = './datasets/CG_binarized/', save_path = './results_w_FairCG/')
